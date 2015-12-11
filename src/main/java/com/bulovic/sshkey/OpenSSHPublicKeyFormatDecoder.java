1dbb20e683c3cedf6f9e8ab38d2d13681f2af78c
Mirko Bulovic <mirko.bulovic@here.com>
2015-12-10 22:03:20 -0500

    Initial commit

---
+++ src/main/java/com/bulovic/sshkey/OpenSSHPublicKeyFormatDecoder.java
@@ -0,0 +1,112 @@
+package com.bulovic.sshkey;
+
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.DSAPublicKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
+
+public class OpenSSHPublicKeyFormatDecoder {
+    private static final String RSA = "RSA";
+    public static final String SSH_RSA = "ssh-rsa";
+    private static final String DSA = "DSA";
+    public static final String SSH_DSS = "ssh-dss";
+
+    private String openSSHKey;
+    private String type;
+    private byte[] publicKeyBytes;
+    private int pointer = 0;
+    private PublicKey publicKey;
+    private String comment;
+
+    public OpenSSHPublicKeyFormatDecoder(String openSSHKey) {
+        this.openSSHKey = openSSHKey;
+
+        // separate parts for the input key
+        String[] openSSHKeyParts = openSSHKey.split(" ");
+        if (openSSHKeyParts.length > 3) {
+            throw new IllegalArgumentException("openSSHKey consists of too many parts");
+        } else if (openSSHKeyParts.length < 2) {
+            throw new IllegalArgumentException("openSSHKey consists of too few parts");
+        }
+
+        // save each part
+        String decorativeType = openSSHKeyParts[0];
+        if (!SSH_RSA.equals(decorativeType) && !SSH_DSS.equals(decorativeType)) {
+            throw new IllegalArgumentException("unsupported key type");
+        }
+        publicKeyBytes = Base64.getDecoder().decode(openSSHKeyParts[1]);
+        if(openSSHKeyParts.length == 3) {
+            comment = openSSHKeyParts[2];
+        }
+
+        // read actual key
+        type = decodeType();
+        if (!type.equals(decorativeType)) {
+            throw new IllegalArgumentException("key type mismatch");
+        }
+        try {
+            switch (type) {
+                case SSH_RSA:
+                    BigInteger e = decodeBigInt();
+                    BigInteger m = decodeBigInt();
+                    RSAPublicKeySpec rsaPublicKeySpec = new RSAPublicKeySpec(m, e);
+                    publicKey = KeyFactory.getInstance(RSA).generatePublic(rsaPublicKeySpec);
+                    break;
+                case SSH_DSS:
+                    BigInteger p = decodeBigInt();
+                    BigInteger q = decodeBigInt();
+                    BigInteger g = decodeBigInt();
+                    BigInteger y = decodeBigInt();
+                    DSAPublicKeySpec dsaPublicKeySpec = new DSAPublicKeySpec(y, p, q, g);
+                    publicKey = KeyFactory.getInstance(DSA).generatePublic(dsaPublicKeySpec);
+                    break;
+                default:
+                    throw new IllegalArgumentException("unknown type " + type);
+            }
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private int decodeLength() {
+        return ((publicKeyBytes[pointer++] & 0xFF) << 24)
+            | ((publicKeyBytes[pointer++] & 0xFF) << 16)
+            | ((publicKeyBytes[pointer++] & 0xFF) << 8)
+            | (publicKeyBytes[pointer++] & 0xFF);
+    }
+
+    private String decodeType() {
+        int len = decodeLength();
+        String type = new String(publicKeyBytes, pointer, len);
+        pointer += len;
+        return type;
+    }
+
+    private BigInteger decodeBigInt() {
+        int len = decodeLength();
+        byte[] bigIntBytes = new byte[len];
+        System.arraycopy(publicKeyBytes, pointer, bigIntBytes, 0, len);
+        pointer += len;
+        return new BigInteger(bigIntBytes);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getOpenSSHKey() {
+        return openSSHKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
---
+++ src/test/java/com/bulovic/sshkey/OpenSSHPublicKeyFormatDecoderTest.java
@@ -0,0 +1,79 @@
+package com.bulovic.sshkey;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class OpenSSHPublicKeyFormatDecoderTest {
+    public static final String RSA_KEY = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOrrZ0oN66Ud2lac26qeJT0g/8W4nla86wtC8jNSWTjbKXwJrl8wN8/6uGm75eXFJCCYEPXhO2zJHubS3sJrF9E0twu75iKP86uJM+ZEnf1mzElSfEMteGfSlZc70DLWtvrLKVQOcmvuh/5MlFSBSJrPDNwYZZIWdOqLcNI7YNeyZuX8wKktmOwaqopdB72qoPF24KtAt0ILhuMq+Y9u7ovqyuZlgUvsYBUxfuJVt2GT3+sLX+in2+ihTsABnfmqW5jdH6aPMMy/LYiJBtq4cf/K+L5MUiZCfbwTSw1mpGePUKB850mR4M0vXnMFkZbSkrWCWTMkzoksP0tEZGxLql user@example";
+    public static final String DSA_KEY = "ssh-dss AAAAB3NzaC1kc3MAAACBAKl6Mn4HS41vdcppjBn4PiMe8EEcyVVpYg6iPwX+EQ82ROm0AscfcsHhfrkuz+kthpvrhiwGB9B6XAcm1iIFitUDI+aF0wEsL91kSbRN4rp82n9laBezR8Sau+TR8RKC+dM1Ak44ylveJCyf5PrY89erXGzGVyx00ftx9TYp8gL9AAAAFQDTQiUj+yg941j2qQcaUKQ1+WaGTQAAAIBAYc00ybt3rwyHFiobF6WuQKbDS346I+G3okK7/AzItmYk7bNuBoSsh+OqNvOkSul9JAB5iCz/c9CRB3174Mhu6v8K+5vdWXV15QWZhPCEyythHajWFevii4qtgLSsM0NSRuZTPrYkmF31KnaVlGvAcTpbz8wJ/M0tM12Qins4AAAAAIASnCfscxXZo0QfyvcbkvBVVDcLSQp+BzEtGFfqacwmBBFO58Lqy36m7aUxRmbxmpdVBqbdl4cu+8l1AQoWHPqknK15ykMyNaPsZCiT2dRfjvDxR0rmmu2yf97d+FDtqD3OMIqZfNXKET3NdPyi565KClnFi6NcOI/znUHsYgLAqQ== user@example";
+    public static final String RSA_KEY_SHORT = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOrrZ0oN66Ud2lac26qeJT0g/8W4nla86wtC8jNSWTjbKXwJrl8wN8/6uGm75eXFJCCYEPXhO2zJHubS3sJrF9E0twu75iKP86uJM+ZEnf1mzElSfEMteGfSlZc70DLWtvrLKVQOcmvuh/5MlFSBSJrPDNwYZZIWdOqLcNI7YNeyZuX8wKktmOwaqopdB72qoPF24KtAt0ILhuMq+Y9u7ovqyuZlgUvsYBUxfuJVt2GT3+sLX+in2+ihTsABnfmqW5jdH6aPMMy/LYiJBtq4cf/K+L5MUiZCfbwTSw1mpGePUKB850mR4M0vXnMFkZbSkrWCWTMkzoksP0tEZGxLql";
+    public static final String DSA_KEY_SHORT = "ssh-dss AAAAB3NzaC1kc3MAAACBAKl6Mn4HS41vdcppjBn4PiMe8EEcyVVpYg6iPwX+EQ82ROm0AscfcsHhfrkuz+kthpvrhiwGB9B6XAcm1iIFitUDI+aF0wEsL91kSbRN4rp82n9laBezR8Sau+TR8RKC+dM1Ak44ylveJCyf5PrY89erXGzGVyx00ftx9TYp8gL9AAAAFQDTQiUj+yg941j2qQcaUKQ1+WaGTQAAAIBAYc00ybt3rwyHFiobF6WuQKbDS346I+G3okK7/AzItmYk7bNuBoSsh+OqNvOkSul9JAB5iCz/c9CRB3174Mhu6v8K+5vdWXV15QWZhPCEyythHajWFevii4qtgLSsM0NSRuZTPrYkmF31KnaVlGvAcTpbz8wJ/M0tM12Qins4AAAAAIASnCfscxXZo0QfyvcbkvBVVDcLSQp+BzEtGFfqacwmBBFO58Lqy36m7aUxRmbxmpdVBqbdl4cu+8l1AQoWHPqknK15ykMyNaPsZCiT2dRfjvDxR0rmmu2yf97d+FDtqD3OMIqZfNXKET3NdPyi565KClnFi6NcOI/znUHsYgLAqQ==";
+    public static final String RSA_KEY_SHORT2 = "AAAAB3NzaC1yc2EAAAADAQABAAABAQDOrrZ0oN66Ud2lac26qeJT0g/8W4nla86wtC8jNSWTjbKXwJrl8wN8/6uGm75eXFJCCYEPXhO2zJHubS3sJrF9E0twu75iKP86uJM+ZEnf1mzElSfEMteGfSlZc70DLWtvrLKVQOcmvuh/5MlFSBSJrPDNwYZZIWdOqLcNI7YNeyZuX8wKktmOwaqopdB72qoPF24KtAt0ILhuMq+Y9u7ovqyuZlgUvsYBUxfuJVt2GT3+sLX+in2+ihTsABnfmqW5jdH6aPMMy/LYiJBtq4cf/K+L5MUiZCfbwTSw1mpGePUKB850mR4M0vXnMFkZbSkrWCWTMkzoksP0tEZGxLql";
+    public static final String DSA_KEY_SHORT2 = "AAAAB3NzaC1kc3MAAACBAKl6Mn4HS41vdcppjBn4PiMe8EEcyVVpYg6iPwX+EQ82ROm0AscfcsHhfrkuz+kthpvrhiwGB9B6XAcm1iIFitUDI+aF0wEsL91kSbRN4rp82n9laBezR8Sau+TR8RKC+dM1Ak44ylveJCyf5PrY89erXGzGVyx00ftx9TYp8gL9AAAAFQDTQiUj+yg941j2qQcaUKQ1+WaGTQAAAIBAYc00ybt3rwyHFiobF6WuQKbDS346I+G3okK7/AzItmYk7bNuBoSsh+OqNvOkSul9JAB5iCz/c9CRB3174Mhu6v8K+5vdWXV15QWZhPCEyythHajWFevii4qtgLSsM0NSRuZTPrYkmF31KnaVlGvAcTpbz8wJ/M0tM12Qins4AAAAAIASnCfscxXZo0QfyvcbkvBVVDcLSQp+BzEtGFfqacwmBBFO58Lqy36m7aUxRmbxmpdVBqbdl4cu+8l1AQoWHPqknK15ykMyNaPsZCiT2dRfjvDxR0rmmu2yf97d+FDtqD3OMIqZfNXKET3NdPyi565KClnFi6NcOI/znUHsYgLAqQ==";
+    public static final String RSA_KEY_SHORT3 = "AAAAB3NzaC1yc2EAAAADAQABAAABAQDOrrZ0oN66Ud2lac26qeJT0g/8W4nla86wtC8jNSWTjbKXwJrl8wN8/6uGm75eXFJCCYEPXhO2zJHubS3sJrF9E0twu75iKP86uJM+ZEnf1mzElSfEMteGfSlZc70DLWtvrLKVQOcmvuh/5MlFSBSJrPDNwYZZIWdOqLcNI7YNeyZuX8wKktmOwaqopdB72qoPF24KtAt0ILhuMq+Y9u7ovqyuZlgUvsYBUxfuJVt2GT3+sLX+in2+ihTsABnfmqW5jdH6aPMMy/LYiJBtq4cf/K+L5MUiZCfbwTSw1mpGePUKB850mR4M0vXnMFkZbSkrWCWTMkzoksP0tEZGxLql user@example";
+    public static final String DSA_KEY_SHORT3 = "AAAAB3NzaC1kc3MAAACBAKl6Mn4HS41vdcppjBn4PiMe8EEcyVVpYg6iPwX+EQ82ROm0AscfcsHhfrkuz+kthpvrhiwGB9B6XAcm1iIFitUDI+aF0wEsL91kSbRN4rp82n9laBezR8Sau+TR8RKC+dM1Ak44ylveJCyf5PrY89erXGzGVyx00ftx9TYp8gL9AAAAFQDTQiUj+yg941j2qQcaUKQ1+WaGTQAAAIBAYc00ybt3rwyHFiobF6WuQKbDS346I+G3okK7/AzItmYk7bNuBoSsh+OqNvOkSul9JAB5iCz/c9CRB3174Mhu6v8K+5vdWXV15QWZhPCEyythHajWFevii4qtgLSsM0NSRuZTPrYkmF31KnaVlGvAcTpbz8wJ/M0tM12Qins4AAAAAIASnCfscxXZo0QfyvcbkvBVVDcLSQp+BzEtGFfqacwmBBFO58Lqy36m7aUxRmbxmpdVBqbdl4cu+8l1AQoWHPqknK15ykMyNaPsZCiT2dRfjvDxR0rmmu2yf97d+FDtqD3OMIqZfNXKET3NdPyi565KClnFi6NcOI/znUHsYgLAqQ== user@example";
+    public static final String RSA_KEY_BROKEN = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOrrZ0oN66Ud2lac26qeJT0g/8W4nla86wtC8jNSWTjbKXwJrl8wN8/6uGm75eXFJCCYEPXhO2zJHubS3sJrF9E0twu75iKP86uJM+ZEnf1mzElSfEMteGfSlZc70DLWtvrLKVQOcmvuh/5MlFSBSJrPDNwYZZIWdOqLcNI7YNeyZuX8wKktmOwaqopdB72qoPF24KtAt0ILhuMq+Y9u7ovqyuZlgUvsYBUxfuJVt2GT3+LX+in2+ihTsABnfmqW5jdH6aPMMy/LYiJBtq4cf/K+L5MUiZCfbwTSw1mpGePUKB850mR4M0vXnMFkZbSkrWCWTMkzoksP0tEZGxLql user@example";
+    public static final String DSA_KEY_BROKEN = "ssh-dss AAAAB3NzaC1kc3MAAACBAKl6Mn4HS41vdcppjBn4PiMe8EEcyVVpYg6iPwX+EQ82ROm0AscfcsHhfrkuz+kthpvrhiwGB9B6XAcm1iIFitUDI+aF0wEsL91kSbRN4rp82n9laBezR8Sau+TR8RKC+dM1Ak44ylveJCyf5PrY89erXGzGVyx00ftx9TYp8gL9AAAAFQDTQiUj+yg941j2qQcaUKQ1+WaGTQAAAIBAYc00ybt3rwyHFiobF6WuQKbDS346I+G3okK7/AzItmYk7bNuBoSsh+OqNvOkSul9JAB5iCz/c9CRB3174Mhu6v8K+5vdWXV15QWZhPCEyythHajWFevii4qtgLSsM0NSRuZTPrYkmF31KnaVlGvAcTpbz8wJ/M0tM12Qins4AAAAAIASnCfscxXZo0QfyvcbkvBVVDcLSQp+BzEtGFfqacwmBBFO58Lqy36m7aUxRmbxmpdVBqbdl4cu+8l1AQoWHPqknK15ykMyNaPsZCiT2dRfjvDxR0rmmu2yf97d+FDtqDOMIqZfNXKET3NdPyi565KClnFi6NcOI/znUHsYgLAqQ== user@example";
+
+    @Test
+    public void testRSA1() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(RSA_KEY);
+        assertEquals(openSSHPublicKeyFormatDecoder.getType(), OpenSSHPublicKeyFormatDecoder.SSH_RSA);
+        assertEquals(openSSHPublicKeyFormatDecoder.getComment(), "user@example");
+    }
+
+    @Test
+    public void testRSA2() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(RSA_KEY_SHORT);
+        assertEquals(openSSHPublicKeyFormatDecoder.getType(), OpenSSHPublicKeyFormatDecoder.SSH_RSA);
+        assertNull(openSSHPublicKeyFormatDecoder.getComment());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testRSA3() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(RSA_KEY_SHORT2);
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testRSA4() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(RSA_KEY_SHORT3);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testRSA5() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(RSA_KEY_BROKEN);
+    }
+
+    @Test
+    public void testDSA1() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(DSA_KEY);
+        assertEquals(openSSHPublicKeyFormatDecoder.getType(), OpenSSHPublicKeyFormatDecoder.SSH_DSS);
+        assertEquals(openSSHPublicKeyFormatDecoder.getComment(), "user@example");
+    }
+
+    @Test
+    public void testDSA2() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(DSA_KEY_SHORT);
+        assertEquals(openSSHPublicKeyFormatDecoder.getType(), OpenSSHPublicKeyFormatDecoder.SSH_DSS);
+        assertNull(openSSHPublicKeyFormatDecoder.getComment());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDSA3() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(DSA_KEY_SHORT2);
+
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDSA4() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(DSA_KEY_SHORT3);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testDSA5() throws Exception {
+        OpenSSHPublicKeyFormatDecoder openSSHPublicKeyFormatDecoder = new OpenSSHPublicKeyFormatDecoder(DSA_KEY_BROKEN);
+    }
+}
